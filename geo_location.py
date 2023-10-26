5cd6a76ee1f010b1f574aef2e42503c0cf9a640a
Marius <Marval_rand@outlook.com>
2023-10-26 15:16:53 +0200

    Test av funksjonalitet i geoLocation klassen.

--- geo_location.py
+++ geo_location.py
@@ -2,5 +2,5 @@ from msg_class import UserData
 # Class to get geolocation (mock implementation)
 class GeoLocation:
     def get_location(self):
-        # Simulating an API call to get geolocation data
+        print("Test")
         return 'Grimstad'
