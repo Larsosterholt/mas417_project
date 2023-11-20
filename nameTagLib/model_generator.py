3f6d323fefe5b120f6af38166ec1c492f4c5fb07
Bob <you@example.com>
2023-11-20 14:41:44 +0100

    Clead up in files

--- nameTagLib/model_generator.py
+++ nameTagLib/model_generator.py
@@ -1,21 +1,23 @@
 import cadquery as cq
-import os
-os.environ['FONTCONFIG_PATH'] = '/etc/fonts/'
 
-
-
-class STLGenerator:
+class modelGenerator:
     def generate_stl(self, user_data):
         print(f"Generating STL file for {user_data.name} based at {user_data.geolocation}")
+
+        # Defining width of the name tag based on the number of characters in the name/city
         width = max(len(user_data.name), len(user_data.geolocation))
 
+        # Create model and text
         model = cq.Workplane("XY").box(width * 6, 50, 2)
         text_name = cq.Workplane("XY", origin=(0, 10, 1)).text(user_data.name, 12, 0.8, font="impact")
         text_geolocation = cq.Workplane("XY", origin=(0, -10, 1)).text(user_data.geolocation, 12, 0.8, font="impact")
 
+        # Make the model and text union
         model = model.union(text_name)
         model = model.union(text_geolocation)
+
+        # Export to file
         filename = "./name_tag.3mf"
-        cq.exporters.export(model, filename)#, tolerance=0.01, angularTolerance=0.01)
+        cq.exporters.export(model, filename)
 
         print(f"3mf file generated: name_tag.stl")
